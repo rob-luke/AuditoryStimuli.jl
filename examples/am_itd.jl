b1ab1bd0682a8785397c49e80286ab0bed127c71
rob-luke <hello@robertluke.net>
2018-07-30 09:31:39 +1000

    Use set_ITD in example

--- examples/am_itd.jl
+++ examples/am_itd.jl
@@ -4,12 +4,10 @@ using AuditoryStimuli
 time = 0:1/48000:1
 cn = correlated_noise(length(time), 2, 1)
 bn = bandpass_noise(cn, 300, 700, 48000)
-mn = amplitude_modulate(bn, 40, 48000)
-im = ITD_modulate(mn, 2, 24, -24, 48000)
-of = set_RMS(im, 0.1)
+an = amplitude_modulate(bn, 40, 48000)
+it = set_ITD(an, -24)
 
-pyplot()
-
-a = plot(time, of, lab = "", xlab = "Time (s)", ylab = "Amplitude")
-b = plot(time, of, lab = map(string,[:left :right]), xlab = "Time (s)", ylab = "", xlims = (0.025, 0.05))
-plot(a, b, size = (800, 400))
+a = plot(time, it, lab = "", xlab = "Time (s)", ylab = "Amplitude", xlims = (0.0, 0.5))
+b = plot(time, it, lab = map(string,[:Left :Right]), xlab = "Time (s)", ylab = "", xlims = (0.025, 0.05))
+plot(a, b, size = (1000, 400))
+savefig("examples/am_itd.png")
